7eb796bff421580375f98a32567133ba7412d016
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2020-11-13 09:24:10 -0300

    rotas

--- web/src/pages/Root.js
+++ web/src/pages/Root.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,  Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Cadastro from './Cadastro';
 import CheckOut from './CheckOut';
-//import Route from '../utils/Route';
+import PetShop from './PetShop';
+import Home from './Home';
+
 
 const PagesRoot = () => {
 
+const quant = [1,2,3,4,5,6,1,34,5,4,58, 9,1,2,3,4,5,6,1,34,5,4,58, 9];
+
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Cadastro}/>     
-        <Route path="/cadastro"  component={ Cadastro} />
-        <Route path="/checkout"  component={ CheckOut} />
-             
-      </Switch>
+        <Route path="/" exact >
+        < Home quant={quant}/>
+        </Route> 
+
+        <Route path="/cadastro">
+        <Cadastro />
+        </Route>
+
+        <Route path="/checkout">
+        <CheckOut/>
+        </Route>
+
+        <Route path="/petshop/:id">
+        <PetShop quant={quant}/>
+          </Route>  
+
     </Router>
   );
 }
